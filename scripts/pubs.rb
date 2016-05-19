3f8fe26c1335664bdeabccd938495ba33e69fe22
Bartosz Kostaniak <bartosz@localhost.localdomain>
2016-05-19 10:45:50 +0200

    Skrypt potrafi usuwać puby, dodatkowo pokazuje odpowiedź HTTP

--- scripts/pubs.rb
+++ scripts/pubs.rb
@@ -5,9 +5,13 @@ def parse_arguments
     OptionParser.new do |opts|
         opts.banner = "Usage: ruby pubs.rb [options]"
 
+        opts.on("-dID", "--delete=ID", "Delete pub with given id") do |p|
+            options[:delete] = p
+        end
         opts.on("-p", "--post", "Create new pub") do |p|
             options[:post] = true
         end
+        
         opts.on("--pub=FILE", "JSON file with pub data") do |p|
             options[:pub] = p
         end
@@ -18,5 +22,8 @@ end
 options = parse_arguments
 
 if options[:post]
-    %x(curl -X POST -d @#{options[:pub]} localhost:3000/pubs --header "Content-Type:application/json")
+    puts %x(curl -s -D - -X POST -d @#{options[:pub]} localhost:3000/pubs -H "Accept: application/json" -H "Content-Type:application/json")
+end
+if options[:delete]
+    puts %x(curl -s -D - -X DELETE localhost:3000/pubs/#{options[:delete]} -H "Accept: application/json" -H "Content-Type:application/json")
 end
