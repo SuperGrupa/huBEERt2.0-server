86fa0d0547d3421bea4ad3fed4bd3353e7d02593
Nitrooos <bartoszkostaniak@gmail.com>
2016-07-24 21:06:57 +0200

    Odpowiednie zagnieżdżenie zasobów

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  resources :notifications
-  resources :subscriptions
-  resources :users
-  resources :events, except: :destroy
-  resources :offers
-  resources :comments
+  resources :pubs do
+    resources :events, except: :destroy
+    resources :offers
+    resources :comments
+  end
+
+  resources :users do
+    resources :notifications
+    resources :subscriptions
+  end
+
   resources :beers, except: :destroy
-  resources :pubs
 end
