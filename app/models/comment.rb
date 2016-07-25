c13a4fcfda82cfc6d73efb2d706e9ebb13b9f738
Nitrooos <bartoszkostaniak@gmail.com>
2016-07-25 14:07:27 +0200

    Dodano testy modelu Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :pub
   belongs_to :user
+
+  validates :text, presence: true, length: { maximum: 160 }
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 5 }
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = comments(:one)
+  end
+
+  test "it's not valid without specifing attributes" do
+    assert_not Comment.new.valid?
+  end
+
+  test "text must be present and up to 160 characters" do
+    @comment.text = ""
+    assert_not @comment.valid?
+
+    @comment.text = "a"*161
+    assert_not @comment.valid?
+  end
+
+  test "rating must be integer from range 1..5" do
+    @comment.rating = 2.5
+    assert_not @comment.valid?
+
+    @comment.rating = 0
+    assert_not @comment.valid?
+
+    @comment.rating = 6
+    assert_not @comment.valid?
+  end
+
+  test "comment from fixture should be valid" do
+    assert @comment.valid?
+  end
 end
