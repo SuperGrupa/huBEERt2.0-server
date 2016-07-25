2f80173206a52a5360305ec5e3f603d6524c9ade
Nitrooos <bartoszkostaniak@gmail.com>
2016-07-25 15:32:29 +0200

    Dodano testy modelu Event

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,17 @@
 class Event < ApplicationRecord
   belongs_to :pub
   has_one :notification, dependent: :destroy
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :date, presence: true
+  validates :description, presence: true, length: { maximum: 300 }
+  validate :date_from_now
+
+  private
+
+    def date_from_now
+      if self.date && self.date < DateTime.now
+        errors.add(:date, "event must be marked with future date")
+      end
+    end
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @event = events(:one)
+  end
+
+  test "it's not valid without specifing attributes" do
+    assert_not Event.new.valid?
+  end
+
+  test "name must be present and up to 50 characters" do
+    @event.name = ""
+    assert_not @event.valid?
+
+    @event.name = "a"*51
+    assert_not @event.valid?
+  end
+
+  test "description must be present and up to 300 characters" do
+    @event.name = ""
+    assert_not @event.valid?
+
+    @event.name = "a"*301
+    assert_not @event.valid?
+  end
+
+  test "date must be present and placed in future" do
+    @event.date = nil
+    assert_not @event.valid?
+
+    @event.date = 3.days.ago
+    assert_not @event.valid?
+  end
+
+  test "event from fixture should be valid" do
+    assert @event.valid?
+  end
 end
