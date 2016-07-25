43ae464edf62d978ce471325d639b57bc8a0604b
Nitrooos <bartoszkostaniak@gmail.com>
2016-07-25 15:39:33 +0200

    Dodano testy modelu Offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,8 @@
 class Offer < ApplicationRecord
   belongs_to :pub
   belongs_to :beer
+
+  validates :value, presence: true,
+                    numericality: { greater_than: 0.0 },
+                    format: { with: /\A\d+\.\d{2}\z/ }
 end
--- test/models/offer_test.rb
+++ test/models/offer_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class OfferTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @offer = offers(:one)
+  end
+
+  test "it's not valid without specifing attributes" do
+    assert_not Offer.new.valid?
+  end
+
+  test "value must be present, with 2 decimal places and greater than 0.0" do
+    @offer.value = nil
+    assert_not @offer.valid?
+
+    @offer.value = "abc"
+    assert_not @offer.valid?
+
+    @offer.value = -1.23
+    assert_not @offer.valid?
+
+    @offer.value = 0.00
+    assert_not @offer.valid?
+
+    @offer.value = 1.2
+    assert_not @offer.valid?
+
+    @offer.value = 1.234
+    assert_not @offer.valid?
+  end
+
+  test "offer from fixture should be valid" do
+    assert @offer.valid?
+  end
 end
