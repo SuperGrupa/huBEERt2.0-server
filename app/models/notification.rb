bc27392b86dfc4a2bb33b0653fc296e445b5d01e
Nitrooos <bartoszkostaniak@gmail.com>
2016-07-25 15:34:46 +0200

    Dodano testy modelu Notification

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,4 +1,6 @@
 class Notification < ApplicationRecord
   belongs_to :event
   has_and_belongs_to_many :users
+
+  validates :message, presence: true, length: { maximum: 160 }
 end
--- test/models/notification_test.rb
+++ test/models/notification_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class NotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @notification = notifications(:one)
+  end
+
+  test "it's not valid without specifing attributes" do
+    assert_not Notification.new.valid?
+  end
+
+  test "message must be present and up to 160 characters" do
+    @notification.message = ""
+    assert_not @notification.valid?
+
+    @notification.message = "a"*161
+    assert_not @notification.valid?
+  end
+
+  test "notification from fixture should be valid" do
+    assert @notification.valid?
+  end
 end
