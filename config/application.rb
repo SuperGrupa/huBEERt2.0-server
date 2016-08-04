517b6a97823bf7923147d15b60dc7a14644be197
Nitrooos <bartoszkostaniak@gmail.com>
2016-08-04 20:02:51 +0200

    Wiadomości o błędach od teraz po polsku

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module HuBEERt
   class Application < Rails::Application
+    config.i18n.default_locale = :pl
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
