7976d6cbed488b1fa807e2bff32c4a96c235b901
Nitrooos <bartoszkostaniak@gmail.com>
2016-08-11 13:45:45 +0200

    Uwierzytelnianie poprzez hasło (logowanie) i token (wylogowanie)

--- app/controllers/tokens_controller.rb
+++ app/controllers/tokens_controller.rb
@@ -1,15 +1,13 @@
 require 'securerandom'
 
 class TokensController < ApplicationController
-  before_action :set_token, only: :destroy
-  before_action :set_user, only: :create
+  wrap_parameters false
+
+  before_action :set_user, :authenticate_by_password, only: :create
+  before_action :set_token, :authenticate_by_token, only: :destroy
 
   # POST /tokens
   def create
-    unless @user.authenticate(params[:password])
-      render json: { password: 'jest nieprawidłowe '}, status: :unprocessable_entity and return
-    end
-
     @token = @user.tokens.build(value: SecureRandom.hex(64), expire: 1.hour.from_now)
 
     if @token.save
@@ -28,16 +26,13 @@ class TokensController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_token
       @token = Token.find(params[:id])
+      if @token.nil? || @token.value != params[:token]
+        unauthorized(token: 'jest nieprawidłowy')
+      end
     end
 
     # Find user based on received login
     def set_user
       @user = User.find_by(login: params[:login])
-      render json: { login: 'jest nieprawidłowy' }, status: :unprocessable_entity unless @user
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def token_params
-      params.require(:login, :password).permit(:login, :password)
     end
 end
